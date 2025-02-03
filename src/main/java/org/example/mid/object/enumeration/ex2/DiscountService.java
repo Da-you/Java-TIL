5d06bb436872b0638de5cb606188a5de45bb0ffd
JoHyunwoo <jhw3874@gmail.com>
2025-02-03 20:51:40 +0900

    java-mid : 타입 안전 열거형 패턴

---
+++ src/main/java/org/example/mid/object/enumeration/ex2/DiscountService.java
@@ -0,0 +1,18 @@
+package org.example.mid.object.enumeration.ex2;
+
+public class DiscountService {
+	public int discount(ClassGrade grade, int price) {
+		int discountPercent = 0;
+
+		if (grade == ClassGrade.BASIC) {
+			discountPercent = 10;
+		} else if (grade == ClassGrade.GOLD) {
+			discountPercent = 20;
+		} else if (grade == ClassGrade.DIAMOND) {
+			discountPercent = 30;
+		} else {
+			System.out.printf("할인X");
+		}
+		return price * discountPercent / 100;
+	}
+}
